471d4771652aa1d05e2e2d7707afb5ddd031f964
Bryan Tjandra <bryan.tjandra@ui.ac.id>
2022-09-09 09:16:29 +0700

    update katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_katalog,
+        'nama': 'Bryan Tjandra'
+    }
+    return render(request, "katalog.html", context)
