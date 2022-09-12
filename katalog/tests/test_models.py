25cde3e3a8ea6a493559a16d9e07f1ca7b867a47
Bryan Tjandra <88226713+bryan273@users.noreply.github.com>
2022-09-12 22:04:57 +0700

    Update test_models.py

--- katalog/tests/test_models.py
+++ katalog/tests/test_models.py
@@ -3,8 +3,8 @@ from katalog.models import CatalogItem
 
 class TestModels(TestCase):
     def setUp(self):
-        CatalogItem.objects.create(item_name="Teman", item_price=10000000, item_stock=1, description="Saya belikan ice cream kamu", rating=9, item_url="https://hokben.com")
+        CatalogItem.objects.create(item_name="Halo", item_price=5000000, item_stock=2, description="Yang beli berkah", rating=9, item_url="https://halo.com")
 
     def test_models(self):
-        temen = CatalogItem.objects.get(item_name="Teman")
-        self.assertEquals(temen.item_name, "Teman")
+        Halo = CatalogItem.objects.get(item_name="Halo")
+        self.assertEquals(Halo.item_name, "Halo")
